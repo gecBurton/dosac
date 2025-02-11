981a7538791f25a9199f38a6bb758c22697807d4
gecBurton <g.e.c.burton@gmail.com>
2025-02-11 12:53:13 +0000

    moved helm to tf

--- infrastrcuture/scw/helm.tf
+++ infrastrcuture/scw/helm.tf
@@ -14,23 +14,23 @@ resource "kubernetes_secret" "app_secret" {
   }
 
   data = {
-    LLM_MODEL = "gpt-4o-mini",
-    LLM_MODEL_PROVIDER = "openai",
-    EMBEDDING_MODEL = "text-embedding-3-large",
-    OPENAI_API_VERSION =  "2024-02-01",
+    LLM_MODEL               = "gpt-4o-mini",
+    LLM_MODEL_PROVIDER      = "openai",
+    EMBEDDING_MODEL         = "text-embedding-3-large",
+    OPENAI_API_VERSION      = "2024-02-01",
     AWS_STORAGE_BUCKET_NAME = scaleway_object_bucket.bucket.name,
-    AWS_S3_ENDPOINT_URL = scaleway_object_bucket.bucket.api_endpoint,
-    APP_HOST = "51.159.112.167",
-    DEBUG= true,
-    SECRET_KEY = "Postgres123!",
-    POSTGRES_HOST = scaleway_rdb_instance.main.endpoint_ip,
-    POSTGRES_PORT = scaleway_rdb_instance.main.endpoint_port,
-    POSTGRES_NAME = scaleway_rdb_database.main.name,
-    POSTGRES_USER = var.postgres_user,
-    POSTGRES_PASSWORD  = scaleway_rdb_instance.main.password,
-    AWS_ACCESS_KEY_ID =  var.access_key,
-    AWS_SECRET_ACCESS_KEY =  var.secret_key,
-    OPENAI_API_KEY = var.openai_api_key
+    AWS_S3_ENDPOINT_URL     = scaleway_object_bucket.bucket.api_endpoint,
+    APP_HOST                = "51.159.112.167",
+    DEBUG                   = true,
+    SECRET_KEY              = "Postgres123!",
+    POSTGRES_HOST           = scaleway_rdb_instance.main.endpoint_ip,
+    POSTGRES_PORT           = scaleway_rdb_instance.main.endpoint_port,
+    POSTGRES_NAME           = scaleway_rdb_database.main.name,
+    POSTGRES_USER           = var.postgres_user,
+    POSTGRES_PASSWORD       = scaleway_rdb_instance.main.password,
+    AWS_ACCESS_KEY_ID       = var.access_key,
+    AWS_SECRET_ACCESS_KEY   = var.secret_key,
+    OPENAI_API_KEY          = var.openai_api_key
   }
 }
 
@@ -119,5 +119,5 @@ resource "kubernetes_service" "ingres" {
 }
 
 output "web_service_external_ip" {
-     value = kubernetes_service.ingres.status[0].load_balancer[0].ingress[0].ip
-   }
+  value = kubernetes_service.ingres.status[0].load_balancer[0].ingress[0].ip
+}
