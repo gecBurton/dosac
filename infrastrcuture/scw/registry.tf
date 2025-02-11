703b5b9683cd35a3720279566f0ae45d13db03f2
gecBurton <g.e.c.burton@gmail.com>
2025-02-11 13:33:00 +0000

    feature/merge-helm-tf

--- infrastrcuture/scw/registry.tf
+++ infrastrcuture/scw/registry.tf
@@ -6,7 +6,3 @@ resource "scaleway_registry_namespace" "docker-registry" {
 output "registry_endpoint" {
   value = scaleway_registry_namespace.docker-registry.endpoint
 }
-
-output "registry_name" {
-  value = scaleway_registry_namespace.docker-registry.name
-}
